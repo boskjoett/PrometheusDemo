07948b9ea7cf82943097e47df10c6e8c2c1967ea
Bo Christian Skjøtt <bo@skjoett.com>
2020-04-12 22:22:47 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using Prometheus;
+
+namespace DataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MetricServer metricServer;
+            Random random = new Random();
+
+            Console.WriteLine("Metrics generator started");
+
+            try
+            {
+                metricServer = new MetricServer(port: 1234);
+                metricServer.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception while creating MetricServer: {ex.Message}");
+                return;
+            }
+
+            Gauge speedGauge = Metrics.CreateGauge("speed", "Speed in km/h");
+            Counter randomCounter = Metrics.CreateCounter("speed_limit_exceeded", "Speed limit exceeded");
+
+            do
+            {
+                double speed = 100 * random.NextDouble();
+                speedGauge.Set(speed);
+
+                if (speed > 80)
+                    randomCounter.Inc();
+
+                Console.WriteLine($"Speed = {speed}");
+
+                Thread.Sleep(random.Next(200, 2000));
+
+            } while (true /* !Console.KeyAvailable */);
+
+            metricServer.Stop();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# PrometheusDemo
-Prometheus demo using .NET Core
+# Description
+
+Prometheus demo.
+
+Running in Docker containers.
+
+Links:
+* https://medium.com/@dale.bingham_30375/net-core-web-api-metrics-with-prometheus-and-grafana-fe84a52d9843
+* https://unravellingsoftwaredevelopment.wordpress.com/hooking-up-prometheus-within-a-net-core-application/
+* https://hub.docker.com/r/prom/prometheus/
+* https://github.com/prometheus-net/prometheus-net
